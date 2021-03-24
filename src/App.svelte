a619d66523f9ae1ae6f164cfc90f5bb4df57a167
Héctor Echiburú <black.shot@live.cl>
2021-03-24 11:11:09 -0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,6 @@
   import { Router } from '@roxi/routify/runtime'
   // eslint-disable-next-line node/no-missing-import
   import { routes } from '../.routify/routes'
-  
-  const config = {
-    urlTransform: { apply: url => `/portfolio${url}`, remove: url => url.replace('/portfolio', '') }
-  }
 </script>
 
-<Router {routes} {config} />
+<Router {routes}/>
